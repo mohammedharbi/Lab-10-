7f1d2ea0b17bf7b7a25ed4066cb417d2364a6028
mohammedharbi <128936996+mohammedharbi@users.noreply.github.com>
2024-12-02 13:28:57 +0300

    Delete JobSeekingSystem directory

--- JobSeekingSystem/src/main/java/com/example/jobseekingsystem/Repository/JobPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobseekingsystem.Repository;
-
-import com.example.jobseekingsystem.Model.JobPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPostRepository extends JpaRepository<JobPost, Integer> {
-}
--- JobSeekingSystem/src/main/java/com/example/jobseekingsystem/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jobseekingsystem.Repository;
-
-import com.example.jobseekingsystem.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
